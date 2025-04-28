944eb6c91f6a1732c4e32d6dfa5359a702884651
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2025-04-28 21:55:12 +0100

    initalise dll

---
+++ dll/src/lib.rs
@@ -0,0 +1,22 @@
+use windows::{ Win32::Foundation::*, Win32::System::SystemServices::*, };
+
+#[unsafe(no_mangle)]
+#[allow(non_snake_case, unused_variables)]
+extern "system" fn DllMain(
+    dll_module: HINSTANCE,
+    call_reason: u32,
+    _: *mut ())
+    -> bool
+{
+    match call_reason {
+        DLL_PROCESS_ATTACH => main(),
+        DLL_PROCESS_DETACH => (),
+        _ => ()
+    }
+
+    true
+}
+
+fn main() {
+
+}
